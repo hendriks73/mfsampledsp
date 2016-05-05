10ff48e33fad57b8b5973b9e800c3714098ccd3a
Hendrik Schreiber <hs@tagtraum.com>
2016-05-05 17:53:32 +0200

    Fixed library loading issues when the classpath contains a + char.

--- src/main/java/com/tagtraum/mfsampledsp/MFNativeLibraryLoader.java
+++ src/main/java/com/tagtraum/mfsampledsp/MFNativeLibraryLoader.java
@@ -129,7 +129,7 @@ public final class MFNativeLibraryLoader {
                 throw new FileNotFoundException("Failed to get URL of " + fullyQualifiedBaseClassName);
             } else {
                 File directory = null;
-                final String path = URLDecoder.decode(url.getPath(), "UTF-8");
+                final String path = decodeURL(url.getPath());
                 if (JAR_PROTOCOL.equals(url.getProtocol())) {
                     final String jarFileName = new URL(path.substring(0, path.lastIndexOf('!'))).getPath();
                     directory = new File(jarFileName).getParentFile();
@@ -169,5 +169,74 @@ public final class MFNativeLibraryLoader {
                     && fileName.startsWith(libName)
                     && fileString.endsWith(ARCHITECTURE_CLASSIFIER + NATIVE_LIBRARY_EXTENSION);
         }
+    }
+
+    /**
+     * Decode % encodings in URLs.
+     * The common {@link java.net.URLDecoder#decode(String, String)} method also converts {@code +} to {@code space},
+     * which is not what we want.
+     *
+     * @param s url
+     * @return decoded URL
+     */
+    private static String decodeURL(final String s) throws UnsupportedEncodingException {
+        boolean needToChange = false;
+        final int numChars = s.length();
+        final StringBuilder sb = new StringBuilder(numChars > 500 ? numChars / 2 : numChars);
+        int i = 0;
+
+        char c;
+        byte[] bytes = null;
+        while (i < numChars) {
+            c = s.charAt(i);
+
+            if (c == '%') {
+                /*
+                 * Starting with this instance of %, process all
+                 * consecutive substrings of the form %xy. Each
+                 * substring %xy will yield a byte. Convert all
+                 * consecutive  bytes obtained this way to whatever
+                 * character(s) they represent in the provided
+                 * encoding.
+                 */
+
+                try {
+
+                    // (numChars-i)/3 is an upper bound for the number
+                    // of remaining bytes
+                    if (bytes == null) {
+                        bytes = new byte[(numChars - i) / 3];
+                    }
+                    int pos = 0;
+
+                    while (((i+2) < numChars) && (c=='%')) {
+                        int v = Integer.parseInt(s.substring(i+1,i+3),16);
+                        if (v < 0) {
+                            throw new IllegalArgumentException("FFNativeLibraryLoader: Illegal hex characters in escape (%) pattern - negative value");
+                        }
+                        bytes[pos++] = (byte) v;
+                        i+= 3;
+                        if (i < numChars) {
+                            c = s.charAt(i);
+                        }
+                    }
+
+                    // A trailing, incomplete byte encoding such as
+                    // "%x" will cause an exception to be thrown
+                    if ((i < numChars) && (c=='%')) {
+                        throw new IllegalArgumentException("FFNativeLibraryLoader: Incomplete trailing escape (%) pattern");
+                    }
+
+                    sb.append(new String(bytes, 0, pos, "UTF-8"));
+                } catch (NumberFormatException e) {
+                    throw new IllegalArgumentException("FFNativeLibraryLoader: Illegal hex characters in escape (%) pattern - " + e.getMessage());
+                }
+                needToChange = true;
+            } else {
+                sb.append(c);
+                i++;
+            }
+        }
+        return needToChange ? sb.toString() : s;
     }
 }
